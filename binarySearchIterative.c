4d62b26bd2aa2bcb9e7b4389d81613b2f0eb263d
Shubham Jagtap <106365555+booleanbug@users.noreply.github.com>
2022-07-16 01:17:03 +0530

    Create binarySearchIterative.c

---
+++ binarySearchIterative.c
@@ -0,0 +1,18 @@
+int search(int* nums, int numsSize, int target){
+    int start = 0;
+    int end = numsSize-1;
+    
+    while(start <= end){
+        int mid = start + (end-start)/2;
+        if(target == nums[mid]){
+            return mid;
+        }
+        if(target > nums[mid]){
+            start = mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+    }
+    return -1;
+}
